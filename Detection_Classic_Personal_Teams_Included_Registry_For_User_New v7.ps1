79c939ab577d236eb116338a0c903cd90ddb83ee
durairaj1991 <durairaj2021@gmail.com>
2024-08-27 09:30:13 +0300

    Add files via upload

---
+++ Detection_Classic_Personal_Teams_Included_Registry_For_User_New v7.ps1
@@ -0,0 +1,181 @@
+﻿<#
+.SYNOPSIS
+This script detects the presence of Microsoft Teams Classic and Personal installations, checks related registry entries for each user profile, and logs the results. It also includes registry checks for the machine-wide installer and personal installations.
+
+.DESCRIPTION
+1. Defines functions to ensure the log directory exists and to log output messages.
+2. Sets up the log file path with a timestamp for uniqueness.
+3. Checks the registry for Teams installations in each user profile by loading the user's registry hive.
+4. Checks for Classic and Personal Teams installations by examining common installation paths.
+5. Logs the results of the registry and installation checks.
+6. Determines and logs the final result based on the detection of Teams installations.
+7. Exits with a success or failure code based on the detection outcome.
+
+.LOGGING
+Logs are created in the directory "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Teams\" with a timestamp in the filename for uniqueness.
+
+.PARAMETERS
+None.
+
+.NOTES
+- Ensure the script is run with administrative privileges to perform the required actions.
+- The script checks for user profile registries by loading each profile's NTUSER.DAT hive, searches for Teams-related registry entries, and logs the findings.
+- It also checks for Teams installations in common directories and logs the presence or absence of the applications.
+- Final results are based on the findings and are logged along with exit codes for Intune.
+#>
+
+
+# Function to ensure log directory exists
+function Ensure-LogDirectory {
+    param (
+        [string]$logFilePath
+    )
+
+    $logDirectory = Split-Path -Path $logFilePath
+
+    if (-not (Test-Path -Path $logDirectory -PathType Container)) {
+        Write-Host "Creating log directory: $logDirectory"
+        New-Item -Path $logDirectory -ItemType Directory -Force | Out-Null
+    }
+}
+
+# Function to log output to file
+function Log-Output {
+    param (
+        [string]$message,
+        [string]$logFilePath
+    )
+    
+    $outputLine = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') $message"
+    Ensure-LogDirectory -logFilePath $logFilePath
+    Add-Content -Path $logFilePath -Value $outputLine
+}
+
+# Define log file path with date and time
+$logFileName = "Detection_Classic_Personal_Teams_Included_Registry_For_User_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
+$logFilePath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Teams\$logFileName"
+
+# Function to check registry for Teams
+function Check-TeamsRegistry {
+    param (
+        [string]$hiveName
+    )
+
+    $registryPaths = @(
+        "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+    )
+
+    foreach ($path in $registryPaths) {
+        $fullPath = "Registry::HKEY_USERS\$hiveName\$path"
+        if (Test-Path -Path $fullPath) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+# Get all user profiles except system profiles and Public/Default profiles
+$AllUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users" | Where-Object {
+    $_.PSIsContainer -and 
+    $_.Name -notlike "Administrator" -and 
+    $_.Name -notlike "ITAdmin" -and 
+    $_.Name -notlike "IT" -and 
+    $_.Name -notlike "Dadmin" -and 
+    $_.Name -notlike "Jadmin" -and 
+    $_.Name -notlike "Eadmin" -and 
+    $_.Name -notlike "Public" -and 
+    $_.Name -notlike "Default*" 
+    
+}
+
+$registryCheck_OtheruserProfiles_UsingHIVE  = @()
+
+foreach ($userProfile in $AllUsers) {
+    $userName = $userProfile.Name
+    $userProfilePath = $userProfile.FullName
+    $ntUserDatPath = "$userProfilePath\NTUSER.DAT"
+
+    if (Test-Path -Path $ntUserDatPath) {
+        $hiveName = "TEMP_HIVE_$userName"
+        
+        try {
+            # Load the user hive
+            reg load "HKEY_USERS\$hiveName" "$ntUserDatPath" 2>&1 | Out-Null
+            Log-Output "Loaded hive for $userName" $logFilePath
+            
+            if (Check-TeamsRegistry -hiveName $hiveName) {
+                Log-Output "Teams installation found for user: $userName" $logFilePath
+                 $registryCheck_OtheruserProfiles_UsingHIVE += "`nHIVE_$userName : SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+            } else {
+                Log-Output "No Teams installation found for user: $userName" $logFilePath
+            }
+        } catch {
+            $errorMessage = $_.Exception.Message
+            Log-Output "Failed to load hive for '$userName': $errorMessage" $logFilePath
+        } finally {
+            # Unload the user hive
+            reg unload "HKEY_USERS\$hiveName" 2>&1 | Out-Null
+            Log-Output "Unloaded hive for $userName`n" $logFilePath
+        }
+    } else {
+        Log-Output "NTUSER.DAT not found for user: $userName`n" $logFilePath
+    }
+}
+
+# Check for Teams installation paths
+$TeamsClassic = @()
+$TeamsPersonal = Get-AppxPackage -Name MicrosoftTeams -AllUsers
+$TeamsNew = Get-ChildItem -Path "C:\Program Files\WindowsApps" -Filter "MSTeams_*"
+
+foreach ($User in $AllUsers) {
+    $userName = $User.Name
+    $localAppData = "$($ENV:SystemDrive)\Users\$userName\AppData\Local\Microsoft\Teams"
+
+    Log-Output "Processing user: $userName" $logFilePath
+
+    if (Test-Path "$localAppData\Current\Teams.exe") {
+        $TeamsClassic += "`n$localAppData\Current\Teams.exe"
+        Log-Output "Classic Teams found for user: $userName" $logFilePath
+    } else {
+        Log-Output "Classic Teams not found for user: $userName" $logFilePath
+    }
+}
+
+# Check if Classic Teams is installed from registry
+$ClassicTeamsRegistry = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Teams" -ErrorAction SilentlyContinue
+
+# Check if Classic Teams is installed from registry Current Users
+$ClassicTeamsRegistry_CurrentUser = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams" -ErrorAction SilentlyContinue
+
+# Check if Teams registry path exists for current user
+$ClassicTeamsRegistry_CurrentUserExists = Test-Path -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+
+# Check for Teams Machine-Wide Installer
+$MachineWideInstallerRegistryPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$MachineWideInstaller = Get-ItemProperty -Path $MachineWideInstallerRegistryPath | Where-Object -Property DisplayName -eq "Teams Machine-Wide Installer"
+
+# Log values of the registry checks
+Log-Output "`n`nClassicTeamsRegistry: $($ClassicTeamsRegistry | Out-String)" $logFilePath
+Log-Output "`n`nregistryCheck_OtheruserProfiles_UsingHIVE: $($registryCheck_OtheruserProfiles_UsingHIVE | Out-String)" $logFilePath
+Log-Output "`n`nClassicTeamsRegistry_CurrentUser: $($ClassicTeamsRegistry_CurrentUser | Out-String)" $logFilePath
+Log-Output "`n`nClassicTeamsRegistry_CurrentUserExists: $ClassicTeamsRegistry_CurrentUserExists" $logFilePath
+Log-Output "`n`nTeams Machine-Wide Installer: $($MachineWideInstaller | Out-String)" $logFilePath
+
+# Log values of the Teams installation paths
+Log-Output "`n`nTeamsClassic: $($TeamsClassic -join ', ')" $logFilePath
+Log-Output "`n`nTeamsPersonal: $($TeamsPersonal | Out-String)" $logFilePath
+Log-Output "`n`nTeamsNew: $($TeamsNew.FullName -join ', ')" $logFilePath
+
+# Determine result based on detection checks
+if (-not $TeamsClassic -and -not $TeamsPersonal -and $TeamsNew -and -not $registryCheck -and -not $ClassicTeamsRegistry -and -not $ClassicTeamsRegistry_CurrentUser -and -not $ClassicTeamsRegistry_CurrentUserExists -and -not $MachineWideInstaller) {
+    Log-Output "`n`nClassic and Personal Teams Not Found, ClassicTeamsRegistry not found, or Teams registry path not found for current user.`nNew Teams Only!" $logFilePath
+    Log-Output "Detection script completed. Log file saved to: $logFilePath" $logFilePath
+    Log-Output "exit 0 # Success exit code for Intune  # Issues Not Detected" $logFilePath
+    #exit 0 # Success exit code for Intune
+} else {
+    Log-Output "`n`nClassic and Personal Teams Found!`nClassicTeamsRegistry found." $logFilePath
+    Log-Output "Detection script completed. Log file saved to: $logFilePath" $logFilePath
+    Log-Output "exit 1 # Failure exit code for Intune  # Issues Detected" $logFilePath
+    #exit 1 # Failure exit code for Intune
+}
---
+++ Remediation_Classic_Personal_Teams_Included_Registry_For Intune_Microsoft_New_v7.ps1
@@ -0,0 +1,248 @@
+# Name: Uninstall-TeamsClassic
+
+# This function receives a Path where the Teams classic application was installed. 
+# If the application was totally uninstalled before, it is expected the Uninstallation will not occur.
+
+# Ensure classic Teams and personal processes are stopped initially, before removal
+$classicTeamsProcesses = Get-Process -Name "Teams" -ErrorAction SilentlyContinue | Where-Object { $_.Path -like "*AppData\Local\Microsoft\Teams*" }
+if ($classicTeamsProcesses) {
+    Write-Host "Stopping all classic Teams processes" -ForegroundColor Yellow
+    $classicTeamsProcesses | Stop-Process -Force
+    Write-Host "Stopped all classic Teams processes" -ForegroundColor Green
+}
+
+$personalTeamsProcesses = Get-Process -Name "msteams" -ErrorAction SilentlyContinue
+if ($personalTeamsProcesses) {
+    Write-Host "Stopping Microsoft Teams Personal app process" -ForegroundColor Yellow
+    $personalTeamsProcesses | Stop-Process -Force
+    Write-Host "Stopped Microsoft Teams Personal app process" -ForegroundColor Green
+}
+
+function Uninstall-TeamsClassic($TeamsPath) {
+    try {
+        $process = Start-Process -FilePath "$TeamsPath\Update.exe" -ArgumentList "--uninstall /s" -PassThru -Wait -ErrorAction STOP
+
+        if ($process.ExitCode -ne 0) {
+            Write-Host "Uninstallation failed:."
+        }
+    } catch {
+        Write-Host "Uninstallation failed:."
+    }
+}
+
+# $AllUsers contains the list of all the users on the server.
+# Get all user profiles except system profiles and Public/Default profiles
+# Get all user profiles except system profiles, Public/Default profiles, and the currently logged-in user
+$AllUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users" | Where-Object {
+    $_.PSIsContainer -and 
+    $_.Name -notlike "Administrator" -and 
+    $_.Name -notlike "ITAdmin" -and 
+    $_.Name -notlike "IT" -and 
+    $_.Name -notlike "Dadmin" -and 
+    $_.Name -notlike "Jadmin" -and 
+    $_.Name -notlike "Eadmin" -and 
+    $_.Name -notlike "Public" -and 
+    $_.Name -notlike "Default*" 
+    
+}
+
+# Loop to uninstall Teams for all the users.
+foreach ($User in $AllUsers) {
+    Write-Host "Processing user: $($User.Name)"
+
+    # Installation folders
+    $localAppData = "$($ENV:SystemDrive)\Users\$($User.Name)\AppData\Local\Microsoft\Teams"
+    $programData = "$($env:ProgramData)\$($User.Name)\Microsoft\Teams"
+
+    if (Test-Path "$localAppData\Current\Teams.exe") {
+        Write-Host "Uninstalling Classic Teams for user $($User.Name)"
+        Uninstall-TeamsClassic -TeamsPath $localAppData
+    } elseif (Test-Path "$programData\Current\Teams.exe") {
+        Write-Host "Uninstalling Classic Teams for user $($User.Name)"
+        Uninstall-TeamsClassic -TeamsPath $programData
+    } else {
+        Write-Host "Teams installation was not found for user $($User.Name)"
+    }
+}
+
+# Remove old Teams folders and icons
+$TeamsFolder_old = "$($ENV:SystemDrive)\Users\*\AppData\Local\Microsoft\Teams"
+$TeamsIcon_old = "$($ENV:SystemDrive)\Users\*\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Teams*.lnk"
+Get-Item $TeamsFolder_old | Remove-Item -Force -Recurse
+Get-Item $TeamsIcon_old | Remove-Item -Force -Recurse
+
+
+# Get the current logged-in user's name
+$loggedInUser = $env:USERNAME
+
+# Get all user profiles except system profiles, Public/Default profiles, and the currently logged-in user
+$AllUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users" | Where-Object {
+    $_.PSIsContainer -and 
+    $_.Name -notlike "Administrator" -and 
+    $_.Name -notlike "ITAdmin" -and 
+    $_.Name -notlike "IT" -and 
+    $_.Name -notlike "Dadmin" -and 
+    $_.Name -notlike "Jadmin" -and 
+    $_.Name -notlike "Eadmin" -and 
+    $_.Name -notlike "Public" -and 
+    $_.Name -notlike "Default*" -and
+    $_.Name -ne $loggedInUser
+}
+
+
+# Delete the Registry keys fro ANother User Profiles
+try {
+    # Create a new PSDrive for HKEY_USERS if it doesn't already exist
+    if (-not (Get-PSDrive HKU -ErrorAction SilentlyContinue)) {
+        New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+    }
+
+    # $users contains the list of all the users
+    $users = $AllUsers
+
+    
+
+       foreach ($user in $users) {
+       
+        $hiveName = "TEMP_HIVE_$user"
+
+        Write-Host "Loading Hive for $user"
+
+        # 20-second wait before loading the registry hive
+        #Write-Host "Start-Sleep -Seconds 20"
+        #Start-Sleep -Seconds 20
+        
+
+        # Attempt to load the user's registry hive
+        try {
+            Write-Host " `n`nLoading Registry : ${user}"
+            Write-Host "Loading Reg Path $user for at HKU:\$hiveName\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+            Start-Process -FilePath "reg" -ArgumentList "load", "hku\$hiveName", "C:\Users\$user\NTUSER.DAT" -NoNewWindow -Wait -ErrorAction Stop
+        } catch {
+            Write-Host "Error: Failed to load hive for ${user}: " -ForegroundColor Red
+            continue
+        }
+        
+        # 40-second wait before checking the registry key
+        Write-Host "Start-Sleep -Seconds 20"
+        Start-Sleep -Seconds 20
+        Write-Host "Start-Sleep -Seconds 20"
+        Start-Sleep -Seconds 20
+
+         Write-Host " `n"
+        # Check for Teams uninstall key and remove it if present
+        try {
+            $registryPath = "HKU:\$hiveName\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+            Write-Host "Checking Reg Path $user for at HKU:\$hiveName\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+            if (Test-Path -Path $registryPath) {
+                Write-Host "Found & Removing Reg $user for at HKU:\$hiveName\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+                Remove-Item -Path $registryPath -Force
+                Write-Host "Removed registry key for user $user"
+            } else {
+                Write-Host " `n"
+                Write-Host "Not Found : Reg Path $user for at HKU:\$hiveName\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Teams"
+            
+            }
+        } catch {
+            Write-Host "Permission Error or Path Error for ${user}: " -ForegroundColor Red
+        }
+
+        # 40-second wait before checking the registry key
+        Write-Host "Start-Sleep -Seconds 20"
+        Start-Sleep -Seconds 20
+        Write-Host "Start-Sleep -Seconds 20"
+        Start-Sleep -Seconds 20
+
+        # Attempt to unload the user's registry hive
+        try {
+            Write-Host " `nUnloading Loading Registry : ${user}"
+            Start-Process -FilePath "reg" -ArgumentList "unload", "hku\$hiveName" -NoNewWindow -Wait -ErrorAction Stop
+            # 20-second wait after unloading the registry hive
+            # 40-second wait before checking the registry key
+        Write-Host "Start-Sleep -Seconds 20"
+        Start-Sleep -Seconds 20
+        } catch {
+            Write-Host "Error: Failed to unload hive for ${user}: " -ForegroundColor Red
+        }
+    }
+} catch {
+    Write-Host "An unexpected error occurred: " -ForegroundColor Red
+}
+
+
+
+
+
+###########################################################
+# Check for Teams Machine-Wide Installer
+###########################################################
+ Write-Host " `n"
+# Remove Teams Machine-Wide Installer
+Write-Host "Removing Teams Machine-wide Installer" -ForegroundColor Yellow
+
+$registryPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$MachineWide = Get-ItemProperty -Path $registryPath | Where-Object -Property DisplayName -eq "Teams Machine-Wide Installer"
+
+if ($MachineWide) {
+    Write-Host "Found: Teams Machine-Wide Installer installed" -ForegroundColor Green
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/x ""$($MachineWide.PSChildName)"" /qn" -NoNewWindow -Wait
+    Write-Host "Waiting 1 minute after uninstallation..." -ForegroundColor Yellow
+    Start-Sleep -Seconds 60  # Delay for 60 seconds
+    Write-Host "Teams Machine-Wide Installer uninstalled successfully" -ForegroundColor Green
+} else {
+    Write-Host "Teams Machine-Wide Installer not found" -ForegroundColor Red
+}
+
+Write-Host "Checking: Teams Machine-Wide Installer registry entry at HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" -ForegroundColor Yellow
+$MachineWideInstallerRegistryPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$MachineWideInstaller = Get-ItemProperty -Path $MachineWideInstallerRegistryPath | Where-Object { $_.DisplayName -eq "Teams Machine-Wide Installer" }
+
+if ($MachineWideInstaller) {
+    Write-Host "Found: Teams Machine-Wide Installer registry entry" -ForegroundColor Green
+    $MachineWideInstallerRegistryPath | ForEach-Object {
+        Remove-Item -Path $_.PSPath -Recurse -Force
+        Write-Host "Removed: Teams Machine-Wide Installer registry entry from $($_.PSPath)" -ForegroundColor Green
+    }
+} else {
+    Write-Host "Not Found: Teams Machine-Wide Installer registry entry" -ForegroundColor Red
+}
+
+# Check if Classic Teams is installed from registry
+Write-Host "Checking: Classic Teams registry entry at HKLM:\SOFTWARE\Microsoft\Teams" -ForegroundColor Yellow
+$ClassicTeamsRegistry = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Teams" -ErrorAction SilentlyContinue
+
+if ($ClassicTeamsRegistry) {
+    Write-Host "Found: Classic Teams registry entries at HKLM:\SOFTWARE\Microsoft\Teams" -ForegroundColor Green
+    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force
+    Write-Host "Removed: Classic Teams registry entries from HKLM:\SOFTWARE\Microsoft\Teams" -ForegroundColor Green
+} else {
+    Write-Host "Not Found: Classic Teams registry entries at HKLM:\SOFTWARE\Microsoft\Teams" -ForegroundColor Red
+}
+
+# Remove the specified registry key from HKEY_LOCAL_MACHINE
+try {
+    $regKeyPath = 'HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{731F6BAA-A986-45A4-8936-7C3AAAAA760B}'
+    
+    if (Test-Path -Path $regKeyPath) {
+        Remove-Item -Path $regKeyPath -Force
+        Write-Host "Removed registry key: $regKeyPath" -ForegroundColor Green
+    } else {
+        Write-Host "Registry key not found: $regKeyPath" -ForegroundColor Yellow
+    }
+} catch {
+    Write-Output("Something went wrong while removing the registry key: $regKeyPath")
+}
+
+# Check if Classic Teams is installed from registry for current user
+Write-Host "Checking: Classic Teams registry entry for current user at HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams" -ForegroundColor Yellow
+$ClassicTeamsCurrentUserRegistry = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams" -ErrorAction SilentlyContinue
+
+if ($ClassicTeamsCurrentUserRegistry) {
+    Write-Host "Found: Classic Teams registry entry for current user" -ForegroundColor Green
+    Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Teams" -Recurse -Force
+    Write-Host "Removed: Classic Teams registry entry for current user" -ForegroundColor Green
+} else {
+    Write-Host "Not Found: Classic Teams registry entry for current user" -ForegroundColor Red
+}
+
+Write-Host "All tasks complete."
